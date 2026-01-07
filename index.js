936cf4672c0ced1f2003b483f207f865e46768ba
Michael J. Manley <mjmanley@wisegs.net>
2026-01-06 16:52:03 -0800

    Add Region Code

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const SECRET_ACCESS_KEY = core.getInput('aws_secret_access_key', {
   required: true,
 });
 const BUCKET = core.getInput('aws_bucket', { required: true });
+const REGION = core.getInput('region', { required: false });
 const SOURCE_DIR = core.getInput('source_dir', { required: true });
 const DESTINATION_DIR = core.getInput('destination_dir', { required: false });
 const ENDPOINT = core.getInput('endpoint', { required: false });
@@ -24,6 +25,7 @@ const s3options = {
     accessKeyId: AWS_KEY_ID,
     secretAccessKey: SECRET_ACCESS_KEY,
   },
+  region: REGION,
 };
 
 if (ENDPOINT) {
